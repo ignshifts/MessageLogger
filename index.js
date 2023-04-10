57d502f66078cb2ecb7c744f05294582f0d0e50f
Shifts <74390871+ignshifts@users.noreply.github.com>
2023-04-10 15:22:20 -0400

    self initial

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// const { Client, WebhookClient, MessageEmbed, } = require('discord.js-selfbot-v13');
-const { Client, Attachment, MessageEmbed, Message, Intents, WebhookClient } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const { Client, WebhookClient, MessageEmbed, } = require('discord.js-selfbot-v13');
+const client = new Client();
 const config = require('./config.json');
 
 if(!config.token) return console.log('No token provided.');
